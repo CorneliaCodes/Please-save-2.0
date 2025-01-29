ea759b628813bf6fb04bd84f02872f130acfa522
CorneliaCodes <corneliahui97@gmail.com>
2025-01-29 09:53:52 +0100

    commited to death of  this project

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+const slides = document.querySelector('.slides');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+
+let index = 0;
+
+function showSlide(direction) {
+  const slideCount = slides.children.length;
+  const slideWidth = slides.children[0].offsetWidth;
+
+  if (direction === 'next') {
+    index = (index + 1) % slideCount;
+  } else if (direction === 'prev') {
+    index = (index - 1 + slideCount) % slideCount;
+  }
+
+  slides.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+next.addEventListener('click', () => showSlide('next'));
+prev.addEventListener('click', () => showSlide('prev'));
